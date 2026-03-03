f31f7956e6eed2d0db942a1a39266479d6ba7d70
Alcanaishere <a1canaph@gmail.com>
2026-03-03 16:30:14 +0900

    Add files via upload

--- sys.py
+++ sys.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+
+import msvcrt
+import random
+
+words=['ヘキセン','ブタン']
+index = 0
+
+
+
+question = words[random.randint(0,1)]
+print(question)
+
+while index < len(question):
+    key = msvcrt.getch().decoe("utf-8")
+
+    print(key, end="", flush=True)
+
+    if key == question[index]:
+        index += 1
+    else:
+        print('\nミス！最初から')
+        index = 0
+
+print('\nクリア!')
